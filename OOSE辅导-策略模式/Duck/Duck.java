5ae89ef412d52bdb78de84b6219488cdf756407d
Daibo Gong <2872067g@student.gla.ac.uk>
2025-04-11 16:19:00 +0100

    添加了火箭鸭的部分

--- OOSE辅导-策略模式/Duck/Duck.java
+++ OOSE辅导-策略模式/Duck/Duck.java
@@ -1,7 +1,7 @@
 public abstract class Duck{
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
-    public Duck(){}
+    public Duck(){}//抽象类不需要初始化状态
     
     public abstract void display();//抽象的方法，留给子类鸭子去决定具体的display实现
 
@@ -16,4 +16,12 @@ public abstract class Duck{
     public void swim(){
         System.out.println("All ducks float, even decoys!");//所有鸭子都游泳，包括诱饵鸭
     }
+
+    public void setFlyBehavior(FlyBehavior fb){
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb){
+        quackBehavior = qb;
+    }//这里两个setter体现了策略模式优势——运行时行为可变更
 }
---
+++ OOSE辅导-策略模式/Duck/FlyRockedPowered.java
@@ -0,0 +1,7 @@
+public class FlyRockedPowered implements FlyBehavior{
+    @Override
+    public void fly(){
+        System.out.println("I'm flying with a rocket!");
+    }
+    
+}
